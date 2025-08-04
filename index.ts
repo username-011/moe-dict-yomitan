00f1a47e2c1759d0b375bde7e4b2e506f94b837b
kurenai-11 <moldrenkailich@gmail.com>
2025-08-04 22:55:38 +0300

    added the revised dic as well

--- index.ts
+++ index.ts
@@ -21,95 +21,136 @@ const someEntry = {
 };
 type Entry = typeof someEntry;
 
-const zhuyinDic = new Dictionary({ fileName: "moe-concised-zhuyin.zip" });
-const pinyinDic = new Dictionary({ fileName: "moe-concised-pinyin.zip" });
-const zhuyinIndex = new DictionaryIndex()
+const zhuyinConcisedDic = new Dictionary({
+  fileName: "moe-concised-zhuyin.zip",
+});
+const pinyinConcisedDic = new Dictionary({
+  fileName: "moe-concised-pinyin.zip",
+});
+const zhuyinRevisedDic = new Dictionary({ fileName: "moe-revised-zhuyin.zip" });
+const pinyinRevisedDic = new Dictionary({ fileName: "moe-revised-pinyin.zip" });
+const zhuyinIndexConcised = new DictionaryIndex()
   .setTitle("國語辭典簡編本 注音")
   .setRevision("1.2")
   .setAuthor("shadow")
   .setAttribution("國語辭典簡編本 (2014)")
   .setDescription(
     "A monolingual dictionary made for learners of Mandarin Chinese. 主要適用對象：國中、小學生及學習華語人士。"
-  )
-  .build();
-const pinyin = new DictionaryIndex()
+  );
+const pinyinIndexConcised = new DictionaryIndex()
   .setTitle("國語辭典簡編本 拼音")
   .setRevision("1.2")
   .setAuthor("shadow")
   .setAttribution("國語辭典簡編本 (2014)")
   .setDescription(
     "A monolingual dictionary made for learners of Mandarin Chinese. 主要適用對象：國中、小學生及學習華語人士。"
-  )
-  .build();
-
-await zhuyinDic.setIndex(zhuyinIndex, "build", "index-zhuyin.json");
-await pinyinDic.setIndex(pinyin, "build", "index-pinyin.json");
-
-const fb = await fs.readFile("dict/dict_concised_2014_20250626.xlsx");
-const workbook = read(fb);
-const sheet = workbook.Sheets[workbook.SheetNames[0]!]!;
-const data = utils.sheet_to_json(sheet) as Entry[];
-console.log(
-  data[7304]?.釋義
-    .split("\n")
-    .map((l) => l.trim())
-    .filter((l) => l.length > 0)
+  );
+await zhuyinConcisedDic.setIndex(
+  zhuyinIndexConcised.build(),
+  "build",
+  "index-concised-zhuyin.json"
 );
+await pinyinConcisedDic.setIndex(
+  pinyinIndexConcised.build(),
+  "build",
+  "index-concised-pinyin.json"
+);
+await zhuyinRevisedDic.setIndex(
+  zhuyinIndexConcised
+    .setTitle("重編國語辭典修訂本 注音")
+    .setRevision("1.0")
+    .setDescription(
+      "A monolingual dictionary made for Mandarin Chinese. 主要適用對象：對歷史語言有興趣的研究者。"
+    )
+    .build(),
+  "build",
+  "index-revised-zhuyin.json"
+);
+await pinyinRevisedDic.setIndex(
+  pinyinIndexConcised
+    .setTitle("重編國語辭典修訂本 拼音")
+    .setRevision("1.0")
+    .setDescription(
+      "A monolingual dictionary made for Mandarin Chinese. 主要適用對象：對歷史語言有興趣的研究者。"
+    )
+    .build(),
+  "build",
+  "index-revised-pinyin.json"
+);
+
+const fbConcised = await fs.readFile("dict/dict_concised_2014_20250626.xlsx");
+const fbRevised = await fs.readFile("dict/dict_revised_2015_20250627.xlsx");
+const workbookConcised = read(fbConcised);
+const workbookRevised = read(fbRevised);
+const sheetConcised = workbookConcised.Sheets[workbookConcised.SheetNames[0]!]!;
+const sheetRevised = workbookRevised.Sheets[workbookRevised.SheetNames[0]!]!;
+const dataConcised = utils.sheet_to_json(sheetConcised) as Entry[];
+const dataRevised = utils.sheet_to_json(sheetRevised) as Entry[];
 const simplifiedConverter = new OpenCC("tw2sp.json");
-let i = 0;
-for (const entry of data) {
-  const {
-    字詞名: term,
-    注音一式: zhuyinReading,
-    變體注音: altZhuyinReading,
-    漢語拼音: pinyinReading,
-    變體漢語拼音: altPinyinReading,
-    釋義,
-  } = entry;
-  const simplifiedTerm = simplifiedConverter.convertSync(term);
-  const trimmedMeaning = (釋義 ?? "")
-    .split("\n")
-    .map((l) => l.trim())
-    .filter((l) => l.length > 0)
-    .join("\n");
-  let adjustedMeaning = `【${term}】 `;
-  if (term !== simplifiedTerm) adjustedMeaning += `【${simplifiedTerm}】 `;
-  const zhuyin = zhuyinReading.trim();
-  const pinyin = pinyinReading.trim();
-  const zhuyinTermEntry = new TermEntry(term)
-    .setReading(zhuyin)
-    .addDetailedDefinition(
-      adjustedMeaning +
-        (altZhuyinReading && altZhuyinReading.trim()
-          ? `變體注音: 【${altZhuyinReading}】\n`
-          : "\n") +
-        trimmedMeaning
-    );
-  const pinyinTermEntry = new TermEntry(term)
-    .setReading(pinyin)
-    .addDetailedDefinition(
-      adjustedMeaning +
-        (altPinyinReading && altPinyinReading.trim()
-          ? `變體漢語拼音: 【${altPinyinReading}】\n`
-          : "\n") +
-        trimmedMeaning
-    );
-  await Promise.all([
-    zhuyinDic.addTerm(zhuyinTermEntry.build()),
-    pinyinDic.addTerm(pinyinTermEntry.build()),
-  ]);
-  if (term !== simplifiedTerm) {
-    zhuyinTermEntry.setTerm(simplifiedTerm);
-    pinyinTermEntry.setTerm(simplifiedTerm);
+let a = 0;
+for (let i = 0; i < 2; i++) {
+  for (const entry of i === 0 ? dataConcised : dataRevised) {
+    const {
+      字詞名: term,
+      注音一式: zhuyinReading,
+      變體注音: altZhuyinReading,
+      漢語拼音: pinyinReading,
+      變體漢語拼音: altPinyinReading,
+      釋義,
+    } = entry;
+    const simplifiedTerm = simplifiedConverter.convertSync(term);
+    const trimmedMeaning = (釋義 ?? "")
+      .split("\n")
+      .map((l) => l.trim())
+      .filter((l) => l.length > 0)
+      .join("\n");
+    let adjustedMeaning = `【${term}】 `;
+    if (term !== simplifiedTerm) adjustedMeaning += `【${simplifiedTerm}】 `;
+    const zhuyinTermEntry = new TermEntry(term)
+      .setReading(zhuyinReading ? zhuyinReading.trim() : "")
+      .addDetailedDefinition(
+        adjustedMeaning +
+          (altZhuyinReading && altZhuyinReading.trim()
+            ? `變體注音: 【${altZhuyinReading}】\n`
+            : "\n") +
+          trimmedMeaning
+      );
+    const pinyinTermEntry = new TermEntry(term)
+      .setReading(pinyinReading ? pinyinReading.trim() : "")
+      .addDetailedDefinition(
+        adjustedMeaning +
+          (altPinyinReading && altPinyinReading.trim()
+            ? `變體漢語拼音: 【${altPinyinReading}】\n`
+            : "\n") +
+          trimmedMeaning
+      );
     await Promise.all([
-      zhuyinDic.addTerm(zhuyinTermEntry.build()),
-      pinyinDic.addTerm(pinyinTermEntry.build()),
+      i === 0
+        ? zhuyinConcisedDic.addTerm(zhuyinTermEntry.build())
+        : zhuyinRevisedDic.addTerm(zhuyinTermEntry.build()),
+      i === 0
+        ? pinyinConcisedDic.addTerm(pinyinTermEntry.build())
+        : pinyinRevisedDic.addTerm(pinyinTermEntry.build()),
     ]);
-  }
-  if (++i % 1000 === 0) {
-    console.log(`Processed ${i} entries`);
+    if (term !== simplifiedTerm) {
+      zhuyinTermEntry.setTerm(simplifiedTerm);
+      pinyinTermEntry.setTerm(simplifiedTerm);
+      await Promise.all([
+        i === 0
+          ? zhuyinConcisedDic.addTerm(zhuyinTermEntry.build())
+          : zhuyinRevisedDic.addTerm(zhuyinTermEntry.build()),
+        i === 0
+          ? pinyinConcisedDic.addTerm(pinyinTermEntry.build())
+          : pinyinRevisedDic.addTerm(pinyinTermEntry.build()),
+      ]);
+    }
+    if (++a % 5000 === 0) {
+      console.log(`Processed ${a} entries`);
+    }
   }
 }
 
-await zhuyinDic.export("build");
-await pinyinDic.export("build");
+await zhuyinConcisedDic.export("build");
+await pinyinConcisedDic.export("build");
+await zhuyinRevisedDic.export("build");
+await pinyinRevisedDic.export("build");
