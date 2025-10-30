e45fe27a2b20e90e1ada8c1622d36c68502fd407
kurenai-11 <moldrenkailich@gmail.com>
2025-10-30 21:58:24 +0300

    fixed the light version

--- export.ts
+++ export.ts
@@ -81,7 +81,7 @@ export async function initDics(): Promise<
   });
   const zhuyinIndexConcised = new DictionaryIndex()
     .setTitle("國語辭典簡編本 注音")
-    .setRevision("2025/10/30a")
+    .setRevision("2025/10/30b")
     .setAuthor("shadow")
     .setAttribution("國語辭典簡編本 (2014)")
     .setDescription(
@@ -98,7 +98,7 @@ export async function initDics(): Promise<
   zhuyinIndexConcised.index.targetLanguage = "zh";
   const pinyinIndexConcised = new DictionaryIndex()
     .setTitle("國語辭典簡編本 拼音")
-    .setRevision("2025/10/30a")
+    .setRevision("2025/10/30b")
     .setAuthor("shadow")
     .setAttribution("國語辭典簡編本 (2014)")
     .setDescription(
@@ -126,7 +126,7 @@ export async function initDics(): Promise<
   await zhuyinRevisedDic.setIndex(
     zhuyinIndexConcised
       .setTitle("重編國語辭典修訂本 注音")
-      .setRevision("2025/10/30a")
+      .setRevision("2025/10/30b")
       .setDescription(
         "A monolingual dictionary made for Mandarin Chinese. 主要適用對象：對歷史語言有興趣的研究者。"
       )
@@ -145,7 +145,7 @@ export async function initDics(): Promise<
   await pinyinRevisedDic.setIndex(
     pinyinIndexConcised
       .setTitle("重編國語辭典修訂本 拼音")
-      .setRevision("2025/10/30a")
+      .setRevision("2025/10/30b")
       .setDescription(
         "A monolingual dictionary made for Mandarin Chinese. 主要適用對象：對歷史語言有興趣的研究者。"
       )
