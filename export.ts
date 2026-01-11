6a18dd9d9035186a9b8c3c5544f822f75855cc12
kurenai-11 <moldrenkailich@gmail.com>
2026-01-11 18:05:45 +0300

    [Liang'an] added color style for ∥ note

--- export.ts
+++ export.ts
@@ -5,7 +5,7 @@ import { addTermsLiangAn } from "./liangan.ts";
 const versions = {
   moeConcised: "2026/01/11.2",
   moeRevised: "2026/01/09.2",
-  liangAn: "2026/01/11.5",
+  liangAn: "2026/01/11.6",
 };
 
 // for the concised and revised moe dictionaries
