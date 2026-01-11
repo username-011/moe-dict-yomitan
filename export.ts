668b826aef72a4035e00f8954f2b6a19c9ef150e
kurenai-11 <moldrenkailich@gmail.com>
2026-01-11 16:54:14 +0300

    fix version numbers

--- export.ts
+++ export.ts
@@ -3,9 +3,9 @@ import { addTermsMoe } from "./moe_dics.ts";
 import { addTermsLiangAn } from "./liangan.ts";
 
 const versions = {
-  moeConcised: "2026/01/11.1",
+  moeConcised: "2026/01/11.2",
   moeRevised: "2026/01/09.2",
-  liangAn: "2026/01/11.1",
+  liangAn: "2026/01/11.2",
 };
 
 // for the concised and revised moe dictionaries
